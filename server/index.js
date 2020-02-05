4ce6d5543c70c639c10aefcc23eb883b6ca63384
Zack Singh <zacharysingh@gmail.com>
2020-02-05 15:18:39 -0800

    Get 2 modules up and running

--- server/index.js
+++ server/index.js
@@ -1,55 +1,28 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/../client/'));
+app.use(express.json())
 app.listen(port, () => { console.log(`Now listening on ${port}`); });
 
-
-/////// Build route handlers \\\\\\\\
-
-
-
-app.get('/zackbundle/', (req, res) => {
-
-  //contact API server for componenent 1
-  axios.get('/localhost:3001')
-    .then((data) => {
-      console.log('Successful GET bundle.js from yourself!');
-      res.status(200).send(data);
-    })
-    .catch(() => {
-      console.log('Error getting bundle.js from you', err);
-      res.status(500).send(err);
-    })
+app.get(/icons/, function(req, res) {
+  res.redirect('http://localhost:3001'+req.url);
 })
 
-app.get('/remibundle/', (req, res) => {
-
-  //contact API server for componenent 2
-  axios.get('/localhost:3002')
-    .then((data) => {
-      console.log('Successful GET bundle.js from remi!');
-      res.status(200).send(data);
+app.get('/reservations', (req, res) => {
+  axios.get('http://localhost:3002/reservations')
+    .then((response) => {
+      console.log(response.data)
+      res.status(200).send(response.data);
     })
-    .catch(() => {
-      console.log('Error getting bundel.js from remi', err);
-      res.status(500).send(err);
-    })
-})
-
-app.get('/colebundle/', (req, res) => {
-
-  //contact API server for componenent 3
-  axios.get('/localhost:3003')
-    .then((data) => {
-      console.log('Successful GET bundle.js from cole!');
-      res.status(200).send(data);
-    })
-    .catch(() => {
-      console.log('Error getting bundle.js from cole', err);
-      res.status(500).send(err);
+    .catch((err) => {
+      res.status(404).end;
     })
 })
