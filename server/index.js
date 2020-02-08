2343edeff2c09c5b21a4bc5ea5e317b9bedd6adb
Zack Singh <zacharysingh@gmail.com>
2020-02-07 16:34:00 -0800

    Update proxy to work with all 3 components

--- server/index.js
+++ server/index.js
@@ -16,10 +16,29 @@ app.get(/icons/, function(req, res) {
   res.redirect('http://localhost:3001'+req.url);
 })
 
+app.get('/dishes-bundle.js', (req, res) => {
+  axios.get('https://yelpfec.s3-us-west-1.amazonaws.com/popular-dishes-bundle/bundle.js')
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).end;
+    })
+})
+
 app.get('/restaurants', (req, res) => {
   axios.get('http://localhost:3001/restaurants')
     .then((response) => {
-      console.log(response.data)
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).end;
+    })
+})
+
+app.get('/reservations-bundle.js', (req, res) => {
+  axios.get('https://yelpfec.s3-us-west-1.amazonaws.com/reservations-bundle/bundle.js')
+    .then((response) => {
       res.status(200).send(response.data);
     })
     .catch((err) => {
@@ -30,7 +49,26 @@ app.get('/restaurants', (req, res) => {
 app.get('/reservations', (req, res) => {
   axios.get('http://localhost:3002/reservations')
     .then((response) => {
-      console.log(response.data)
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).end;
+    })
+})
+
+app.get('/gallery-bundle.js', (req, res) => {
+  axios.get('https://yelpfec.s3-us-west-1.amazonaws.com/gallery-bundle/bundle.js')
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).end;
+    })
+})
+
+app.get('/gallery', (req, res) => {
+  axios.get('http://localhost:3003/images')
+    .then((response) => {
       res.status(200).send(response.data);
     })
     .catch((err) => {
